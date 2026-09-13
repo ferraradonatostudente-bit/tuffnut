d292c426f89cdf257d5cbe2e080ecac8e39febb2
Ferrara Donato <donato.ferrara@example.com>
2026-09-13 21:26:38 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("button");
+const consiglio = document.getElementById("consiglio");
+
+button.addEventListener("click", function() {
+  consiglio.textContent =
+    "Consiglio: con Rhinar cerca il momento giusto per sorprendere l'avversario con un attacco enorme.";
+});
